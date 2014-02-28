99e15501a3067be53b9457f97a5ceff451cf7c59
Ben Parnell <benjaminparnell.94@gmail.com>
2014-02-28 09:35:05 +0000

    Parens and nested parens working

--- Numbers/Worker.java
+++ Numbers/Worker.java
@@ -14,35 +14,40 @@ public class Worker extends NumbersBaseListener {
     public void exitMod(NumbersParser.ModContext ctx) {
       int right = memory.pop();
       int left = memory.pop();
-      System.out.println("\n\nResult: " + (left % right));
+      memory.push(left % right);
     }
 
     @Override
     public void exitMul(NumbersParser.MulContext ctx) {
       int right = memory.pop();
       int left = memory.pop();
-      System.out.println("\n\nResult: " + (left * right));
+      memory.push(left * right);
     }
 
     @Override
     public void exitSub(NumbersParser.SubContext ctx) {
       int right = memory.pop();
       int left = memory.pop();
-      System.out.println("\n\nResult: " + (left - right));
+      memory.push(left - right);
     }
 
     @Override
     public void exitAdd(NumbersParser.AddContext ctx) {
       int right = memory.pop();
       int left = memory.pop();
-      System.out.println("\n\nResult: " + (left + right));
+      memory.push(left + right);
     }
 
     @Override
     public void exitDiv(NumbersParser.DivContext ctx) {
       int right = memory.pop();
       int left = memory.pop();
-      System.out.println("\n\nResult: " + (left / right));
+      memory.push(left / right);
+    }
+
+    @Override
+    public void exitProgram(NumbersParser.ProgramContext ctx) {
+      System.out.println("\n\n" + memory.pop());
     }
 
     @Override
